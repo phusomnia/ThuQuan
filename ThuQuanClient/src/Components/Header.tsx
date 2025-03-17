b1f326e6a95b7f04c11a76033c527b07074ccd44
senseiikuiku <dduc1171@gmail.com>
2025-03-17 14:16:42 +0700

    Revert "FE_Csh"
    This reverts commit e80df188a4a5aa2a6d0664b5ae20205cfcbe3a02.

---
+++ ThuQuanClient/src/Components/Header.tsx
@@ -0,0 +1,9 @@
+const Header = () => {
+    return (
+        <>
+        <h1>Header: React Router Tutorial</h1>
+        </>
+    )
+}
+
+export {Header}
---
+++ ThuQuanClient/src/Pages/Contact.tsx
@@ -0,0 +1,12 @@
+import { Header } from "../Components/Header";
+
+const Contact = () => {
+    return(
+    <>
+        <Header/>
+        <h2>Contact Page</h2>
+    </>
+    )
+}
+
+export {Contact}
